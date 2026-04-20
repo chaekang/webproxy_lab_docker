e8c1a7bf8a42019a574d04735476835ce321819e
chaekang <herry612@naver.com>
2026-04-20 10:39:13 +0900

    adder 수정

--- webproxy-lab/tiny/cgi-bin/adder.c
+++ webproxy-lab/tiny/cgi-bin/adder.c
@@ -11,14 +11,14 @@ int main(void)
   int n1 = 0, n2 = 0;
 
   /* Extract the two arguments */
-  if ((buf = getenv("QUERY_STRING")) != NULL)
+  if ((buf = getenv("QUERY_STRING")) != NULL)  // QUERY_STRING 환경 변수에서 인자들을 읽어옴, CGI 프로그램이 인자들을 QUERY_STRING 환경 변수에서 읽을 수 있도록 설정했기 때문
   {
-    p = strchr(buf, '&');
-    *p = '\0';
-    strcpy(arg1, buf);
-    strcpy(arg2, p + 1);
-    n1 = atoi(strchr(arg1, '=') + 1);
-    n2 = atoi(strchr(arg2, '=') + 1);
+    p = strchr(buf, '&');  // buf에서 '&' 문자를 찾아서 p에 저장, '&' 문자는 두 인자 사이를 구분하는 문자이기 때문
+    *p = '\0'; // '&' 문자를 '\0'으로 바꿔서 buf를 두 개의 문자열로 나눔, 예: "arg1=123&arg2=456" -> "arg1=123\0arg2=456"
+    strcpy(arg1, buf); // arg1에 첫 번째 인자를 저장, 예: "arg1=123"을 arg1에 저장
+    strcpy(arg2, p + 1);  // arg2에 두 번째 인자를 저장, 예: "arg2=456"을 arg2에 저장
+    n1 = atoi(arg1);
+    n2 = atoi(arg2);
   }
 
   /* Make the response body */
